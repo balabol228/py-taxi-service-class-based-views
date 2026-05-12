53614abd744751130a2e884a9eadee9d11af4f9f
Denys Baliuk <denuk14228@gmail.com>
2026-05-13 00:58:36 +0300

    first try done project taxi-service-class-based-views

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from django.db.models import QuerySet
 from django.shortcuts import render
 
 from taxi.models import Driver, Car, Manufacturer
@@ -13,3 +16,36 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    context_object_name = "manufacturer_list"
+    template_name = "taxi/manufacturer_list.html"
+    queryset = Manufacturer.objects.order_by("name")
+    paginate_by = 5
+    
+    
+class CarListView(generic.ListView):
+    model = Car
+    paginate_by = 5
+    
+    def get_queryset(self) -> QuerySet:
+        return Car.objects.select_related("manufacturer")
+      
+      
+class CarDetailView(generic.DetailView):
+    model = Car
+    
+    
+class DriverListView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+    
+    
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    
+    def get_queryset(self) -> QuerySet:
+        return Driver.objects.prefetch_related("cars__manufacturer")
